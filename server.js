6e7df17cfe6c9320565032a1d20ef99186116d49
yeptony <thomasgrosjean00@gmail.com>
2017-01-13 19:35:59 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var app = express()
+var MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('link-to-mongodb', function(err, database) {
+  
+})
+
+app.get('/', function (req, res) {
+  res.end("Hi!")
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('App running!')
+})
